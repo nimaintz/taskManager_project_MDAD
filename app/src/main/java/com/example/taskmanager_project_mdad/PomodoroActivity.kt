88429d78ab5d6d63dbb949ebf5393fd2ba3e303e
Rebeca <163349981+ChioreanRebeca@users.noreply.github.com>
2024-12-28 17:41:11 +0200

    The other pages have been created. They are still mostly empty. I will go over them after I configure the main page.

---
+++ app/src/main/java/com/example/taskmanager_project_mdad/PomodoroActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taskmanager_project_mdad
+
+class PomodoroActivity {
+    //To Do
+}
---
+++ app/src/main/java/com/example/taskmanager_project_mdad/SettingsActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taskmanager_project_mdad
+
+class SettingsActivity {
+    //To Do
+}
