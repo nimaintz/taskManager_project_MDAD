02ab5968cce312c98f07b5db7e95ecba4654b030
Rebeca <163349981+ChioreanRebeca@users.noreply.github.com>
2024-12-28 17:33:28 +0200

    I added necessary components for the main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.taskmanager_project_mdad"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.taskmanager_project_mdad"
