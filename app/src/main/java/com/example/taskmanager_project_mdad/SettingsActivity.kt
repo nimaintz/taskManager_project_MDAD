107903d369762df81b1a5620f10ac62e46d9e557
Rebeca <163349981+ChioreanRebeca@users.noreply.github.com>
2024-12-29 23:52:34 +0200

    solved the issue with settings feature
    it crashed because the view objects were being initialized before the setContentView(--yourViewLayout--)
    🐜

--- app/src/main/java/com/example/taskmanager_project_mdad/SettingsActivity.kt
+++ app/src/main/java/com/example/taskmanager_project_mdad/SettingsActivity.kt
@@ -7,20 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
-    val textSize1 = findViewById<TextView>(R.id.text)
-    val seekBar1 = findViewById<SeekBar>(R.id.seek_bar)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_main)
 
-        /*seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+        var textSizeVisual = findViewById<TextView>(R.id.text_size_visual)
+        val seekBar1 = findViewById<SeekBar>(R.id.seek_bar)
+
+        seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                // Set the text size based on the SeekBar progress
-                // Make sure to convert progress to float (and optionally scale it)
-                // You can apply a scaling factor to adjust the text size range
-                val textSize = progress.toFloat() // Directly use progress for text size
-                textSize1.textSize = textSize
+                val size = progress.toFloat()
+                textSizeVisual.textSize = size
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -30,7 +31,7 @@ class SettingsActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 // Optional: Handle when the user stops interacting with the SeekBar
             }
-        })*/
+        })
     }
 
 
